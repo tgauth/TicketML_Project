90c260993f4c194642f9fb075af58d6495b0ff09
tgauth <tgauth@bu.edu>
2019-04-09 21:52:00 -0400

    Add files via upload

---
+++ TicketML.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  9 21:35:35 2019
+
+@author: Tess
+"""
+
+import pandas as pd
+
+df = pd.read_csv("Traffic_Tickets_Issued_Four_Year_Window.csv")
