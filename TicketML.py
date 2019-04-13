82f14f2ff7c3e9b904fc9cf37f27c35167624dce
tgauth <tgauth@bu.edu>
2019-04-13 17:34:34 -0400

    graphs

--- TicketML.py
+++ TicketML.py
@@ -5,32 +5,53 @@ Created on Tue Apr  9 21:35:35 2019
 
 @author: Tess
 """
-
+from collections import Counter
 import pandas as pd, numpy as np, datetime as dt
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
-Data=pd.read_csv('Traffic_Tickets_Issued_Four_Year_Window.csv')
+Data=pd.read_csv('Traffic_Violations.csv')
 
 #Convert Date to day of week Monday=0
 Data['Date Of Stop']=pd.to_datetime(Data['Date Of Stop']).dt.dayofweek
 
 #Convert time to Day time(Morning, Evening,etc)
+#Morning=np.ndarray.tolist(pd.date_range("6:00", "12:00", freq="1min").time)
+#Afternoon=np.ndarray.tolist(pd.date_range("12:01", "17:00", freq="1min").time)
+#Evening=np.ndarray.tolist(pd.date_range("17:01", "21:00", freq="1min").time)
+#Night=np.ndarray.tolist(pd.date_range("21:01", "5:00", freq="1min").time)
+#Time=Data['Time Of Stop'].tolist()
+#newTime=[]
+#for t in Time:
+#    if t in Morning:
+#        newTime.append('Morning')
+#    elif t in Afternoon:
+#        newTime.append('Afternoon')
+#    elif t in Evening:
+#        newTime.append('Evening')
+#    else:
+#        newTime.append('Night')
+#        
+#Data['Time Of Stop']=newTime
 
-Morning=np.ndarray.tolist(pd.date_range("6:00", "12:00", freq="1min").time)
-Afternoon=np.ndarray.tolist(pd.date_range("12:01", "17:00", freq="1min").time)
-Evening=np.ndarray.tolist(pd.date_range("17:01", "21:00", freq="1min").time)
-Night=np.ndarray.tolist(pd.date_range("21:01", "5:00", freq="1min").time)
-Time=Data['Time Of Stop'].tolist()
-newTime=[]
-for t in Time:
-    if t in Morning:
-        newTime.append('Morning')
-    elif t in Afternoon:
-        newTime.append('Afternoon')
-    elif t in Evening:
-        newTime.append('Evening')
-    else:
-        newTime.append('Night')
-        
-Data['Time Of Stop']=newTime
-    
+# Feature Visualization 
+# intended for features with a small number of options (i.e. yes or no)
+
+featureList = ['Accident', 'Belts', 'Personal Injury',
+               'Property Damage', 'Commercial License', 'HAZMAT',
+               'Commercial Vehicle', 'Work Zone', 'State',
+               'Contributed to Accident', 'Driver City', 'Driver State', 
+               'DL State', 'Arrest Type']
+
+#featureList = ['Alcohol', 'Race', 'Gender', 'Fatal']
+#featureList = ['Agency', 'SubAgency', 'Description']
+#featureList = ['Location', 'Latitude', 'Longitude', 'Geolocation']
+#featureList = ['Vehicle Type', 'Year', 'Make', 'Model']
+
+for feature in featureList:
+    count = Counter(Data[feature])
+    plt.bar(range(len(count)), list(count.values()), align='center')
+    plt.xticks(range(len(count)), list(count.keys()))
+    plt.title(feature)
+    plt.savefig("{}.png".format(feature))
